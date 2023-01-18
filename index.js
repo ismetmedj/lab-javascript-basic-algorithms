42bac77ad05c09c7b08b15cf05a6636f63316df9
Ismet Medjebeur <i.medjebeur@live.fr>
2023-01-18 15:09:46 +0100

    1st iteration done

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Iteration 1: Names and Input
+const hacker1 ="Ismet";
+console.log(`The driver's name is ${hacker1}`);
+
+const hacker2 ="Phu";
+console.log(`The navigator's name is ${hacker2}`);
 
 
 // Iteration 2: Conditionals
